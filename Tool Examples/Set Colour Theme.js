2bb05e24649e0da7a3e1954ec0eae37469b95c75
NikRpk <49123781+NikRpk@users.noreply.github.com>
2022-07-12 16:38:40 +0200

    Cleaned the code to increase readability

--- Tool Examples/Set Colour Theme.js
+++ Tool Examples/Set Colour Theme.js
@@ -1,23 +1,27 @@
 // This sets the theme colours for any spreadsheet where you run it!
 
 function setHF_ColourScheme() {
-    var app = SpreadsheetApp;
-    var spreadsheet = app.getActiveSpreadsheet();
-    var text = app.newColor().setRgbColor("#000000").build();
-    var background = app.newColor().setRgbColor("#ffffff").build();
-    var accent1 = app.newColor().setRgbColor("#96dc14").build();
-    var accent2 = app.newColor().setRgbColor("#009646").build();
-    var accent3 = app.newColor().setRgbColor("#ff5f64").build();
-    var accent4 = app.newColor().setRgbColor("#d9d9d9").build();
-    var accent5 = app.newColor().setRgbColor("#00a0e6").build();
-    var accent6 = app.newColor().setRgbColor("#1464ff").build();
+  var app = SpreadsheetApp;
+  var ss = app.getActiveSpreadsheet();
+  var colour_set = ss.getSpreadsheetTheme().setConcreteColor
+  var set_type = app.ThemeColorType
+  var newColor = app.newColor().setRgbColor
 
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.TEXT, text);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.BACKGROUND, background);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT1, accent1);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT2, accent2);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT3, accent3);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT4, accent4);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT5, accent5);
-    spreadsheet.getSpreadsheetTheme().setConcreteColor(app.ThemeColorType.ACCENT6, accent6);
+  var text = newColor("#000000").build();
+  var background = newColor("#ffffff").build();
+  var accent1 = newColor("#96dc14").build();
+  var accent2 = newColor("#009646").build();
+  var accent3 = newColor("#ff5f64").build();
+  var accent4 = newColor("#d9d9d9").build();
+  var accent5 = newColor("#00a0e6").build();
+  var accent6 = newColor("#1464ff").build();
+
+  colour_set(set_type.TEXT, text);
+  colour_set(set_type.BACKGROUND, background);
+  colour_set(set_type.ACCENT1, accent1);
+  colour_set(set_type.ACCENT2, accent2);
+  colour_set(set_type.ACCENT3, accent3);
+  colour_set(set_type.ACCENT4, accent4);
+  colour_set(set_type.ACCENT5, accent5);
+  colour_set(set_type.ACCENT6, accent6);
 };
