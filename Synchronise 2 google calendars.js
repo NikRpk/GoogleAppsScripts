9e00e4f93780e25b2e9261617c6ece94298a9b94
NikRpk <49123781+NikRpk@users.noreply.github.com>
2024-11-27 11:11:34 +0100

    Update Synchronise 2 google calendars.js

--- Synchronise 2 google calendars.js
+++ Synchronise 2 google calendars.js
@@ -1,4 +1,13 @@
 // Credits go to Will Roman - https://medium.com/@willroman/auto-block-time-on-your-work-google-calendar-for-your-personal-events-2a752ae91dab
+/* Calendar Sync
+This is a mini tool that copies events from one calendar into another one. This can be useful if you have a work calendar and personal one and want to block time between the two without having to fully move into only one calendar.
+
+---- Instructions ----
+1. Create a new Apps Script.
+2. Please copy the contents of the sync.js file into that file.
+3. Set a trigger to run this script every few minutes (would suggest 10 mins).
+*/
+
 
 function sync() {
   var primary_cal = "CALENDAR_ID_1"
