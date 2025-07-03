9a46ef31950f8e4b795704373419489972c6c6f9
NikRpk <49123781+NikRpk@users.noreply.github.com>
2025-07-03 16:25:17 +0200

    Added automatic height calculator to box
    Added

--- Supporting functions/popup with download info.js
+++ Supporting functions/popup with download info.js
@@ -1,20 +1,21 @@
+/**
  * Shows a custom dialog with a success message and a clickable link to the folder.
  * @param {string} fileName The name of the created file.
  * @param {string} folderUrl The URL of the folder containing the file.
  */
 function downloadFilePopup(msg, folderUrl) {
-  // 1. Create a template object from the HTML file.
+  // Create a template object from the HTML file.
   const htmlTemplate = HtmlService.createTemplateFromFile('98. Popup box.html');
 
-  //msg = msg.replace(/\n/g, '<br>');
-
-  // 2. Attach the data to the template. These variables will be available
+  // Attach the data to the template. These variables will be available
   //    as scriplets inside the HTML file.
   htmlTemplate.folderUrl = folderUrl;
-  htmlTemplate.msg = msg.replace(/\n/g, '<br>');;
-  const boxSize = Math.max(180, msg.length + 30);
+  htmlTemplate.msg = msg.replace(/\n/g, '<br>');    // Replace the /n (page breaks) with <br> element tha signals a break in html.
 
-  // 3. Evaluate the template to get the final HTML content and set dimensions.
+  // Heuristic values for calculating height. You can adjust these as needed.
+  const boxSize = calculatePopupBoxHeightInPx(msg);
+
+  // Evaluate the template to get the final HTML content and set dimensions.
   const htmlOutput = htmlTemplate.evaluate()
     .setWidth(400)
     .setHeight(boxSize);
@@ -22,4 +23,32 @@ function downloadFilePopup(msg, folderUrl) {
   Logger.log(`Download popup shown to user. Message: "${msg}" and folder link: ${folderUrl}.`)
   // 4. Show the dialog.
   SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Export Successful');
-}
+}; 
+
+/**
+ * Calculate the size of the popup box based on number of characters, expected lines as a result and line breaks.
+ * @param {string} msg    The message that will populate the popup box
+ * @returns {number}      The height of the popup in pixels
+ */
+function calculatePopupBoxHeightInPx(msg) {
+  const avgCharsPerLine = 48;  // Estimated characters per line in a 400px wide box.
+  const lineHeight = 22;       // Estimated pixel height of a single line of text.
+  const baseHeight = 70;      // Base height for padding, title, the link, and buttons.
+
+  let calculatedHeight = baseHeight; 
+  
+  const messageLines = msg.split('\n');
+  messageLines.forEach(line => {
+    // Find the number of lines based on text wrapping. Add in line breaks.
+    const lineLen = line.length;
+
+    if (lineLen > 0) {
+      calculatedHeight += Math.ceil(line.length / avgCharsPerLine) * lineHeight;
+    } else {
+      calculatedHeight += lineHeight
+    }
+  });
+
+  const boxSize = Math.max(90, Math.min(calculatedHeight, 600));
+  return boxSize
+};
